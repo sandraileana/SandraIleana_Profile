07d6d7724edffce7bef3eba6f996655eb4a472e8
Sandra Ileana <sandraileana@gmail.com>
2021-07-04 00:27:52 -0500

    update 2

--- assests/script.js
+++ assests/script.js
@@ -12,12 +12,43 @@
 
 function myFunction1(){
     var AboutMe = document.getElementById('container1');
+    var MyWork = document.getElementById('container2');
+    var Coding = document.getElementById('container3');
     if (AboutMe.style.display === 'none'){
         AboutMe.style.display = 'block';
+        MyWork.style.display = 'none';
+        Coding.style.display = 'none';
     } else {
         AboutMe.style.display = 'none';
     }
 }
+
+function myFunction2(){
+    var AboutMe = document.getElementById('container1');
+    var MyWork = document.getElementById('container2');
+    var Coding = document.getElementById('container3');
+    if (MyWork.style.display === 'none'){
+        MyWork.style.display = 'block';
+        AboutMe.style.display = 'none';
+        Coding.style.display = 'none';
+    } else {
+        MyWork.style.display = 'none';
+    }
+}
+
+function myFunction3(){
+    var AboutMe = document.getElementById('container1');
+    var MyWork = document.getElementById('container2');
+    var Coding = document.getElementById('container3');
+    if (Coding.style.display === 'none'){
+        Coding.style.display = 'block';
+        AboutMe.style.display = 'none';
+        MyWork.style.display = 'none';
+    } else {
+        Coding.style.display = 'none';
+    }
+}
+
 
     // ;
     // var AboutMeButton = document.getElementById('AboutMeButton');
